a40c1407632291915f85de5a9ba62fc1707b2a23
demoulin7 <bosonhiggs@outlook.fr>
2023-02-06 23:46:31 +0100

    bdd

---
+++ migrations/Version20230206223916.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230206223916 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE actualite (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, titre VARCHAR(255) NOT NULL, contenu LONGTEXT NOT NULL, auteur VARCHAR(255) NOT NULL, date DATE NOT NULL, INDEX IDX_5492819779F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commentaire (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, id_actualite_id INT NOT NULL, contenu LONGTEXT NOT NULL, auteur VARCHAR(255) NOT NULL, date DATE NOT NULL, INDEX IDX_67F068BC79F37AE5 (id_user_id), INDEX IDX_67F068BC81454501 (id_actualite_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE signalement (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, id_commentaire_id INT NOT NULL, date DATE NOT NULL, INDEX IDX_F4B5511479F37AE5 (id_user_id), INDEX IDX_F4B5511487FA6C96 (id_commentaire_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE actualite ADD CONSTRAINT FK_5492819779F37AE5 FOREIGN KEY (id_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC79F37AE5 FOREIGN KEY (id_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC81454501 FOREIGN KEY (id_actualite_id) REFERENCES actualite (id)');
+        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B5511479F37AE5 FOREIGN KEY (id_user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B5511487FA6C96 FOREIGN KEY (id_commentaire_id) REFERENCES commentaire (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actualite DROP FOREIGN KEY FK_5492819779F37AE5');
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC79F37AE5');
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC81454501');
+        $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B5511479F37AE5');
+        $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B5511487FA6C96');
+        $this->addSql('DROP TABLE actualite');
+        $this->addSql('DROP TABLE commentaire');
+        $this->addSql('DROP TABLE signalement');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}

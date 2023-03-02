110fe374f5c26024e281ea09c83c8b2f672c194e
Fatima Zohra Bakali <bakalifatimazahrae87@gmail.com>
2023-03-02 20:48:06 +0100

    réglage des droits de supp et de modif d'actual

--- src/Entity/Actualite.php
+++ src/Entity/Actualite.php
@@ -6,6 +6,7 @@ use App\Repository\ActualiteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ActualiteRepository::class)]
@@ -111,6 +112,17 @@ class Actualite
 
         return $this;
     }
+
+
+    /**
+     * Check if the user owns this actualite.
+     */
+    public function belongsToUser(UserInterface $user): bool
+    {
+        return $this->getIdUser() === $user->getId();
+    }
+
+
 
     /**
      * @return Collection<int, Commentaire>

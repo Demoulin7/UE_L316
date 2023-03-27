4011f374bc3448a7356a0825c4109603a099b99d
Fatima Zohra Bakali <bakalifatimazahrae87@gmail.com>
2023-03-27 10:45:48 +0200

    réglage des routes pour l'hébergement

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/uel316/public', name: 'app_home')]
     public function index(ActualiteRepository $actualiteRepository): Response
     {
         return $this->render('home/index.html.twig', [

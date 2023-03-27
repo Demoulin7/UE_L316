ca8e79ffc83b188ecc7916d3b01487630c81634f
FZB <79930498+fatima-zohra-2000@users.noreply.github.com>
2023-03-27 10:06:09 +0200

    Update CommentaireController.php

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/commentaire')]
+#[Route('/uel316/public/commentaire')]
 class CommentaireController extends AbstractController
 {
     #[Route('/', name: 'app_commentaire_index', methods: ['GET'])]
